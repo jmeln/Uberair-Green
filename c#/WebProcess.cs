eae9b4a993e55641aced242c29b4054196e272f5
Matt Schnider <matt.schnider@gmail.com>
2017-10-20 14:50:41 -0400

    WebProcess.cs

---
+++ c#/WebProcess.cs
@@ -0,0 +1,17 @@
+using System; 
+using System.IO;
+using System.Web;
+public class WebProcess
+{
+    public static void Main()
+    {
+        try
+        {
+            Uri reader = new
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("WEB URL NOT FOUND.");
+        }
+    }
+}
