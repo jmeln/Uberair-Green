25ecf3a7976f1081c7dc233440e8951e0cd35f99
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-11-09 00:07:41 -0500

    Passenger can be implemented. Need city class for further progress

--- c#/Passenger.cs
+++ c#/Passenger.cs
@@ -1,29 +1,26 @@
+//Author: Jarrett Melnick
+//Class to store data relating to individual passengers.
 public class Passenger{
-	//TODO: Declare class variables. Set to private
 	private string firstName;
 	private string lastName;
 	private int locationStart;
 	private int locationEnd;
 	private int distTravel;
 	private int priceCharged;
-	//TODO: Build Constructors
 	//Default Constructor
 	public Passenger(string fName, string lName, int locS, int locE){
 		firstName = fName;
 		lastName = lName;
 	}
-	//Intermediate constructors
-	//Full field constructor
-	
 	//TODO: Build Methods necessary
 	//Function to calculate the distance travel
-	private void Distance(int locS, int locE){
-		
+	private int Distance(int locS, int locE){
+		return 0;	
 	}
 	private int CalcPrice(int distT){
-		
+		return 0;	
 	}
-	//TODO: Build Getters and Setters for each variable
+	//Returns the name of the customer
 	public string getName(){
 		return firstName + " " + lastName;
 	}
