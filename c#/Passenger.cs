209412e2b37c67470a682fd476a82a386c0ddb08
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-11-15 23:50:33 -0500

    TESTING Passenger.cs

--- c#/Passenger.cs
+++ c#/Passenger.cs
@@ -1,35 +1,36 @@
 //Author: Jarrett Melnick
 //Class to store data relating to individual passengers.
 public class Passenger{
-	private string firstName;
-	private string lastName;
-	private int locationStart;
-	private int locationEnd;
-	private int distTravel;
-	private int priceCharged;
+	private string _name;
+	private City _cityStart;
+	private City _cityEnd;
+	private int _distTraveled;
+	private int _priceCharged;
 	//Default Constructor
-	public Passenger(string fName, string lName, int locS, int locE){
-		firstName = fName;
-		lastName = lName;
+	public Passenger(string fName, string lName, City locS, City locE){
+		_name = lName + ", " + fName;
+		_cityStart = locS;
+		_cityEnd = locE;
+		Distance(_cityStart)
 	}
 	//TODO: Build Methods necessary
 	//Function to calculate the distance travel
-	private int Distance(int locS, int locE){
+	private static int Distance(City locS, City locE){
 		return 0;	
 	}
 	private int CalcPrice(int distT){
 		return 0;	
 	}
 	//Returns the name of the customer
-	public string getName(){
-		return firstName + " " + lastName;
+	public string Name{
+		get{return _name;}
 	}
 	//Returns the distance the customer is to travel.
-	public int getDistanceTraveled(){
-		return distTravel;
+	public int DistanceTraveled{
+		get{return _distTraveled;}
 	}
 	//Returns the price charged to the individual customer. 
-	public int getPrice(){
-		return priceCharged;
+	public int Price{
+		get{ return _priceCharged;}
 	}
 }
