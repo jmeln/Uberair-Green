5544d05f9765262b9a38f5b551a08742df714c2c
Matt Schnider <matt.schnider@gmail.com>
2017-12-03 20:30:46 -0500

    City class working

---
+++ python/City.py
@@ -0,0 +1,10 @@
+class City:
+    def __init__(self,cityName,state,latitude,longitude,timeZone, sunSet, passengersWaiting):
+        self.CityName = cityName
+        self.State = state
+        self.Latitude = latitude
+        self.Longitude = longitude
+        self.TimeZone = timeZone
+        self.SunSet = sunSet
+        self.PassengersWaiting = passengersWaiting
+
