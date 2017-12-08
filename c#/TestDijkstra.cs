c45d61015a4f0934024dae3dff5e8162d63c4dd4
Emily Herron <emily.joyce.herron@live.mercer.edu>
2017-12-07 19:01:36 -0500

    dijkstra working for longitude and latitude coordinates

--- c#/TestDijkstra.cs
+++ c#/TestDijkstra.cs
@@ -18,6 +18,33 @@ class TestDijkstra
  
         Console.WriteLine(Map_Math.distance(1.0, 15.0, 2.0, 56.0));
    
+        City[] cities = new City[5];
+        cities[0] = new City("Holtsville", "A", 40.922326, -72.637078, 1, 1);
+        cities[1] = new City("Adjuntas", "A", 18.165273,  -66.722583, 1, 1);
+        cities[2] = new City("Aguada", "B", 18.393103, -67.180953, 1, 1);
+        cities[3] = new City("Maricao", "C", 18.172947, -66.944111, 1, 1);
+        cities[4] = new City("Anasco", "D", 18.288685, -67.139696, 1, 1);
+
+
+        DijkstraGraph city_graph = new DijkstraGraph();
+
+        for (int c = 0; c < cities.Length; c++){
+            
+            Dictionary<string, double> dict_temp = new Dictionary<string, double>();
+            for(int d = 0; d < cities.Length; d++){
+                while (d != c){
+                    dict_temp.Add(cities[d].CityName, 
+                        Map_Math.distance(cities[c].Longitude, cities[c].Latitude, cities[d].Longitude, cities[d].Latitude));
+                    break;
+                }
+            }
+
+            city_graph.add_vertex(cities[c].CityName, dict_temp);
+        }
+
+        city_graph.shortest_path("Holtsville", "Anasco").ForEach( x => Console.WriteLine(x) );
+
+
     }
 
 }
